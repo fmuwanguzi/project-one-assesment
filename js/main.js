dd08fbfe8bc3492c29da359b02e0712168953e30
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-12 11:33:38 -0500

    style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
-function increment() {
+function up() {
     document.getElementById('Input').stepUp(1);
  }
- function decrement() {
+ function down() {
     document.getElementById('Input').stepDown(1);
  }
+
+const numAbove = document.querySelector('first')
+
+function show(one){
+    one =parseFloat(one)
+    document.getElementById('first').innerText = (one)
+}
